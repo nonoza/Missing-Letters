065e2698aa2b84a8d140f3d2625883892489bb13
nonoza <32060984+nonoza@users.noreply.github.com>
2018-01-12 08:53:30 +0200

    Add files via upload

---
+++ missing letter.js
@@ -0,0 +1,33 @@
+function MissingLetter(str){
+
+//Looping through the characters
+
+for(var i = 0; i <str.length;i++){
+
+//code of current character which is a.
+
+var code = str.charCodeAt(i);
+
+//If the code of the first character is not equal to the first character plus the iteration.
+
+if(code !== str.charCodeAt(0) + i){
+
+
+//Find the character and return it
+
+return String.fromCharCode(code -1);
+
+   
+ }
+
+
+}
+
+//Return undefined
+
+     return undefined;
+
+
+  };
+ 
+MissingLetter("stvy");
